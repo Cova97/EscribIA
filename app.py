85ab5b2991f6a186907150cf3afd430bc46a6bf7
KODA97 <acovamartinez@gmail.com>
2023-05-11 10:00:06 -0600

    Correct and Tip
    Ya pude corregir y dar un tip en una app.route

--- app.py
+++ app.py
@@ -12,8 +12,16 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/', methods=['POST'])
-def correct_english():
-    text = request.json['text']
+def process():
+    data = request.get_json()
+    text = data['text']
+
+    corrected_text = correct_spanish(text)
+    grade_text = grade(text)
+
+    return jsonify({'corrected_text': corrected_text, 'grade_text': grade_text})
+
+def correct_spanish(text):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=f"Correct this to standard Spanish:\n\n{text}",
@@ -24,12 +32,25 @@ def correct_english():
         presence_penalty=0.0
     )
     corrected_text = response.choices[0].text.strip()
-    return jsonify({'corrected_text': corrected_text})
+    return corrected_text
+
+def grade(text):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"Imagina que eres un evaluador de textos y debes calificar un texto. Basándote en los siguientes aspectos, asigna una calificación (excelente, muy bien, regular, malo) al texto y proporciona una breve justificación para cada calificación:Coherencia y organización: Evalúa si el texto tiene una estructura lógica y coherente, si los párrafos se conectan entre sí y si las ideas se presentan de manera ordenada.Claridad y concisión: Observa si las ideas son expresadas de forma clara y comprensible, evitando ambigüedades y redundancias innecesarias.Desarrollo de argumentos: Analiza si los argumentos presentados son sólidos, relevantes y están respaldados por evidencias convincentes.Uso adecuado del vocabulario: Verifica si el texto utiliza un vocabulario apropiado y variado, evitando repeticiones excesivas y palabras inapropiadas.Ortografía y gramática: Considera si el texto está libre de errores ortográficos y gramaticales, y si se sigue una estructura gramatical correcta.Recuerda justificar cada calificación con base en los aspectos mencionados.:\n\n{text}",
+        temperature=0.5,
+        max_tokens=1500,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    grade_text = response.choices[0].text.strip()
+    return grade_text
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-####################### VERSION CONSOLA ####################
+########################################### VERSION CONSOLA Y OTROS #############################################
 # import os
 # import openai
 # from dotenv import load_dotenv
@@ -53,4 +74,102 @@ if __name__ == '__main__':
 # text = input('promp: ')
 # corrected_text = correct_english(text)
 # print(corrected_text)
+
+# import os
+# from flask import Flask, request, jsonify
+# import openai
+# from dotenv import load_dotenv
+
+# load_dotenv()
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# app = Flask(__name__)
+
+# @app.route('/correct_spanish', methods=['POST'])
+# def correct_spanish():
+#     data = request.get_json()
+#     text = data['text']
+#     response = openai.Completion.create(
+#         model="text-davinci-003",
+#         prompt=f"Correct this to standard Spanish:\n\n{text}",
+#         temperature=0.5,
+#         max_tokens=1500,
+#         top_p=1.0,
+#         frequency_penalty=0.0,
+#         presence_penalty=0.0
+#     )
+#     corrected_text = response.choices[0].text.strip()
+#     return jsonify({'corrected_text': corrected_text})
+
+# @app.route('/grade_text', methods=['POST'])
+# def grade_text():
+#     data = request.get_json()
+#     text = data['text']
+#     response = openai.Completion.create(
+#         model="text-davinci-003",
+#         prompt=f"Imagina que eres un evaluador de textos y debes calificar un texto. Basándote en los siguientes aspectos, asigna una calificación (excelente, muy bien, regular, malo) al texto y proporciona una breve justificación para cada calificación:\n\n{text}",
+#         temperature=0.5,
+#         max_tokens=1500,
+#         top_p=1.0,
+#         frequency_penalty=0.0,
+#         presence_penalty=0.0
+#     )
+#     grade_text = response.choices[0].text.strip()
+#     return jsonify({'grade_text': grade_text})
+
+# if __name__ == '__main__':
+#     app.run()
+
+############################################################################################
+
+# import os
+# import openai
+# from dotenv import load_dotenv
+# from flask import Flask, request, jsonify
+
+# load_dotenv()
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# app = Flask(__name__)
+
+# @app.route('/', methods=['POST'])
+# def process():
+#     data = request.get_json()
+#     text = data['text']
+
+#     corrected_text = correct_spanish(text)
+#     grade_text = grade(text)
+
+#     return jsonify({'corrected_text': corrected_text, 'grade_text': grade_text})
+
+# def correct_spanish(text):
+#     response = openai.Completion.create(
+#         model="text-davinci-003",
+#         prompt=f"Correct this to standard Spanish:\n\n{text}",
+#         temperature=0.4,
+#         max_tokens=1500,
+#         top_p=1.0,
+#         frequency_penalty=0.0,
+#         presence_penalty=0.0
+#     )
+#     corrected_text = response.choices[0].text.strip()
+#     return corrected_text
+
+# def grade(text):
+#     response = openai.Completion.create(
+#         model="text-davinci-003",
+#         prompt=f"Imagina que eres un evaluador de textos y debes calificar un texto. Basándote en los siguientes aspectos, asigna una calificación (excelente, muy bien, regular, malo) al texto y proporciona una breve justificación para cada calificación:Coherencia y organización: Evalúa si el texto tiene una estructura lógica y coherente, si los párrafos se conectan entre sí y si las ideas se presentan de manera ordenada.Claridad y concisión: Observa si las ideas son expresadas de forma clara y comprensible, evitando ambigüedades y redundancias innecesarias.Desarrollo de argumentos: Analiza si los argumentos presentados son sólidos, relevantes y están respaldados por evidencias convincentes.Uso adecuado del vocabulario: Verifica si el texto utiliza un vocabulario apropiado y variado, evitando repeticiones excesivas y palabras inapropiadas.Ortografía y gramática: Considera si el texto está libre de errores ortográficos y gramaticales, y si se sigue una estructura gramatical correcta.Recuerda justificar cada calificación con base en los aspectos mencionados.:\n\n{text}",
+#         temperature=0.5,
+#         max_tokens=1500,
+#         top_p=1.0,
+#         frequency_penalty=0.0,
+#         presence_penalty=0.0
+#     )
+#     grade_text = response.choices[0].text.strip()
+#     return grade_text
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
 
